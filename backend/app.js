ea8be177065bbe70100947f18ddf8da923913944
ErwinRomelLee <erwin@mytalentx.com>
2018-04-09 19:46:27 -0700

    update 18-04-10

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,19 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
 var logger = require('morgan');
 var index = require('./routes/index');
 var app = express();
-
+var server = require('http').Server(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(require('connect-history-api-fallback'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
