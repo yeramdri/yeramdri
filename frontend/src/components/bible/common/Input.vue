e6dc1c9ce772c17440a71c450e8f3acf62452c42
Abel ko <esung1129@gmail.com>
2018-06-28 02:10:43 +0900

    input컴포넌트 추가1

---
+++ frontend/src/components/bible/common/Input.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="input-box shadow">
+        <input type="text">
+        <span class="add-container">
+            <i class="fas fa-search add-btn"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Input'
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.input-box {
+  background: green;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.input-box input {
+  border-style: none;
+  font-size: 1.4rem;
+}
+.add-container {
+  float: right;
+  background: linear-gradient(to right, #339af0, #74c0fc);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.add-btn {
+  color: white;
+  vertical-align: middle;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+</style>
--- frontend/src/components/bible/index.vue
+++ frontend/src/components/bible/index.vue
@@ -15,8 +15,8 @@
 
 <script>
 import BibleHeader from './BibleHeader.vue'
-import All from './All.vue'
-import Subject from './Subject.vue'
+import All from './all'
+import Subject from './subject'
 export default {
   name: 'Bible',
   data () {
@@ -42,4 +42,14 @@ export default {
 </script>
 
 <style>
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
