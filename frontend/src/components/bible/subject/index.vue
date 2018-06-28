657673d9dc9d3156c1487c4e2d21a85d644c90b3
Abel ko <esung1129@gmail.com>
2018-06-29 01:06:53 +0900

    subject에 input 컴포넌트 추가 및 dummy contents로 grid 잡기

--- frontend/src/components/bible/subject/index.vue
+++ frontend/src/components/bible/subject/index.vue
@@ -1,22 +1,71 @@
 <template>
+  <div class="subject-container">
     <div class="subject">
-        subject 야
-        <input type="text">
+      <Input/>
+      <div class="dommy-contents-container">
+        <div class="dommy-contents-row">
+          <div class="dommy-contents">
+            1
+          </div>
+          <div class="dommy-contents">
+            2
+          </div>
+          <div class="dommy-contents">
+            3
+          </div>
+        </div>
+        <div class="dommy-contents-row">
+          <div class="dommy-contents">
+            4
+          </div>
+          <div class="dommy-contents">
+            5
+          </div>
+          <div class="dommy-contents">
+            6
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Input from '../common/Input.vue'
 export default {
-  name: 'Subject'
+  name: 'Subject',
+  components: {
+    Input
+  }
 }
 </script>
 
 <style scoped>
 @import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);
 @media (max-width: 991px) {
+  .subject-container {
+    height: 91vh;
+  }
   .subject {
     margin: 60px auto 0px auto;
-    width: 100%;
+    width: 95vw;
+    padding-top: 3vh;
+    padding-bottom: 3vh;
+  }
+  .dommy-contents-container {
+    padding-top: 1vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .dommy-contents-row {
+    margin-bottom: 3px;
+  }
+  .dommy-contents {
+    display: inline-block;
+    background-color: green;
+    width: 30vw;
+    height: 18vh;
   }
 }
 </style>
