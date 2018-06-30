6aafd5c5baf2984d55991616eaaa7eb2e4bd3999
Abel ko <esung1129@gmail.com>
2018-07-01 04:16:39 +0900

    nav bar에 가로스크롤, hover시, 색깔 바뀌게

--- frontend/src/components/dailyLife/common/Topics.vue
+++ frontend/src/components/dailyLife/common/Topics.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="topics-container">
         <div class="topic">
+            <i class="fas fa-search topic-search"></i>
+            <p>Search</p>
+        </div>
+        <div class="topic">
             <i class="fas fa-check-square"></i>
             <p>True or False<br>in church</p>
         </div>
@@ -29,19 +33,36 @@ export default {
     background-color: #141414;
     margin-top: 50px;
     height: 86px;
-    color: white;
+    color: #f6f6f6;
     display: flex;
-    justify-content: center;
+    /* justify-content: center; */
     padding-top: 12px;
     padding-bottom: 12px;
+
+    overflow: auto;
+    white-space: nowrap;
   }
   .topic {
     display: inline-block;
-    padding-left: 18px;
-    padding-right: 18px;
-    padding-top:10px;
+    /* padding-left: 18px;
+    padding-right: 18px; */
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 10px;
     width: 90px;
     text-align: center;
+
+    display: inline-block;
+    text-decoration: none;
+  }
+  .topic:first-child {
+    padding-right: 25px !important;
+  }
+  .topic:first-child > p {
+    margin-left: 15px;
+  }
+  .topic:hover {
+    color: #777;
   }
   .topic > i {
     font-size: 40px;
