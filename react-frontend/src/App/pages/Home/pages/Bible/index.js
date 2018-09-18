73ff2807b3169b7bc0555d852783085cd66f02c0
livelikeabel <esung1129@gmail.com>
2018-09-19 01:43:19 +0900

    input css 구현

--- react-frontend/src/App/pages/Home/pages/Bible/index.js
+++ react-frontend/src/App/pages/Home/pages/Bible/index.js
@@ -1,20 +1,55 @@
-import React, { Component } from 'react';
-import classnames from 'classnames/bind';
+import React, { Component } from 'react'
+import classnames from 'classnames/bind'
 
-import css from './index.scss';
-const cx = classnames.bind(css);
-const moduleName = 'Room';
+import bibleHeaderImg from 'src/App/pages/Home/assets/bible-header.jpg'
 
-
+import css from './index.scss'
+const cx = classnames.bind(css)
+const moduleName = 'Bible'
 
 class Bible extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className={cx(`${moduleName}`)}>
-        바이블 페이지 입니다.
-    </div>
-    );
+        <div className={cx(`${moduleName}-head`)}>
+          <img src={bibleHeaderImg} alt="bible-header-img" />
+          <h1>BIBLE</h1>
+          <h3>예수, 나를 향한 사랑의 시작</h3>
+        </div>
+        <div className={cx(`${moduleName}-search`)}>
+          <form>
+            <div className={cx(`${moduleName}-search-inputWrapper`)}>
+              <input placeholder="Search Bible" />
+            </div>
+            <div className={cx(`${moduleName}-search-icon`)}>
+              <i className="fas fa-search" />
+            </div>
+          </form>
+        </div>
+        <div className={cx(`${moduleName}-mainVideo`)}>
+          <iframe
+            title="introduceVideo"
+            src="https://www.youtube.com/embed/9xmdxhnIDT8"
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+          <h3>YERAMDRI - 삶의 고백(A Confession of Life)</h3>
+        </div>
+        <div className={cx(`${moduleName}-recentContents`)}>
+          <h3>최근 컨텐츠</h3>
+          <div className={cx(`${moduleName}-contentsBox`)}>
+            <div>컨텐츠1</div>
+            <div>컨텐츠2</div>
+            <div>컨텐츠3</div>
+            <div>
+              <i className="fas fa-chevron-down" />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-export default Bible;
+export default Bible
