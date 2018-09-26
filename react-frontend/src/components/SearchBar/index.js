9ec69fe656f300bb477574e9a286a7ac365acee8
livelikeabel <esung1129@gmail.com>
2018-09-26 16:11:42 +0900

    redirect when keypress submit input

--- react-frontend/src/components/SearchBar/index.js
+++ react-frontend/src/components/SearchBar/index.js
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classnames from 'classnames/bind'
 import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router'
 
 import css from './index.scss'
 const cx = classnames.bind(css)
 const moduleName = 'SearchBar'
 
-const SearchBar = props => {
-  const { onChange, onSubmit, path = '/bible/results' } = props
+class SearchBar extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className={cx(`${moduleName}`)}>
-      <form onSubmit={onSubmit}>
+    this.state = {
+      redirect: null
+    }
+  }
+
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.props.onSubmit()
+      this.setState({ redirect: { to: '/bible/results' } })
+    }
+  }
+
+  render() {
+    const { onChange, onSubmit, path = '/bible/results' } = this.props
+
+    if (this.state.redirect) {
+      return <Redirect {...this.state.redirect} />
+    }
+
+    return (
+      <div className={cx(`${moduleName}`)}>
         <div className={cx(`${moduleName}-inputWrapper`)}>
-          <input onChange={onChange} placeholder="Search Bible" name="search" />
+          <input
+            onChange={onChange}
+            onKeyPress={this.handleKeyPress}
+            placeholder="Search Bible"
+            name="search"
+          />
         </div>
         <Link to={path} onClick={onSubmit} className={cx(`${moduleName}-icon`)}>
           <i className="fas fa-search" />
         </Link>
-      </form>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default SearchBar
