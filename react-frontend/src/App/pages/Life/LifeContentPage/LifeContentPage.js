aa9f7428b7e7affafc5f5afbc1e31ee54bbb4a43
livelikeabel <esung1129@gmail.com>
2018-12-18 17:32:20 +0900

    [#9] Add LifeContentPage css

--- react-frontend/src/App/pages/Life/LifeContentPage/LifeContentPage.js
+++ react-frontend/src/App/pages/Life/LifeContentPage/LifeContentPage.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react'
+import classnames from 'classnames/bind'
 import axios from 'axios'
 
 import { getCurrentContentId } from 'src/utils/contentsUtils'
+import Content from 'src/components/Content'
+
+import css from './LifeContentPage.scss'
+
+const cx = classnames.bind(css)
+const moduleName = 'LifeContentPage'
 
 class LifeContentPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      content: null
+    }
+  }
+
   componentDidMount() {
     this.getLifeContent(getCurrentContentId(document.URL))
   }
 
   getLifeContent(id) {
     axios.get(`http://localhost:6508/life-card/result/${id}`).then(res => {
-      const { data : [ content ] } = res
+      const {
+        data: [content]
+      } = res
       this.setState({ content })
     })
   }
 
+  // TODO: 삶, 말씀 에서 사용중이다. util로 분리 하자. className같은건 어떻게 해야할까..?
+  renderTags = tag => {
+    return tag.split(',').map((tag, index) => {
+      return <span key={index} className={cx(`${moduleName}-tag`)}>#{tag} &nbsp;</span>
+    })
+  }
+
   render() {
-    return <div>LifeContentPage</div>
+    const { content } = this.state
+    return content ? (
+      <div className={cx(`${moduleName}`)}>
+        <div className={cx(`${moduleName}-ContentWrapper`)}>
+          {content.multiMedia.map((media, id) => (
+            <Content media={media} key={id} />
+          ))}
+        </div>
+        <div className={cx(`${moduleName}-postWrapper`)}>
+          <h3 className={cx(`${moduleName}-title`)}>{content.title}</h3>
+          <p className={cx(`${moduleName}-content`)}>{content.content}</p>
+          {this.renderTags(content.tag)}
+        </div>
+      </div>
+    ) : (
+      <h1>Loading</h1>
+    )
   }
 }
 
