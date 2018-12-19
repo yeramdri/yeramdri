c7e7b2e0ca585f2477fd0c546b25e0053950b3cb
livelikeabel <esung1129@gmail.com>
2018-12-19 20:32:52 +0900

    [#12] Add Sidebar (mobile)

--- react-frontend/src/App/index.js
+++ react-frontend/src/App/index.js
@@ -1,21 +1,40 @@
 import React, { Component, Fragment } from 'react'
 import { hot } from 'react-hot-loader'
 import { Switch, Route, withRouter } from 'react-router-dom'
+import Sidebar from 'react-sidebar'
 
 import Header from 'src/components/Header/Header'
 
 import routes from './routes'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sidebarOpen: false
+    }
+  }
+
+  onSetSidebarOpen = open => {
+    this.setState({ sidebarOpen: open })
+  }
+
   render() {
     return (
       <Fragment>
-        <Header />
-        <Switch>
-          {routes.map(({ ...routeProps }) => (
-            <Route {...routeProps} key={routeProps.path || ''} />
-          ))}
-        </Switch>
+        <Sidebar
+          sidebar={<b>Sidebar content</b>}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: 'rgba(0, 0, 0, 0.8)' } }}
+        >
+          <Header toggleSidebar={() => this.onSetSidebarOpen(true)} />
+          <Switch>
+            {routes.map(({ ...routeProps }) => (
+              <Route {...routeProps} key={routeProps.path || ''} />
+            ))}
+          </Switch>
+        </Sidebar>
       </Fragment>
     )
   }
--- react-frontend/src/App/pages/Main/index.js
+++ react-frontend/src/App/pages/Main/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames/bind';
 
-// import MainContent from './components/MainContent';
-// import MainIntroduce from './components/MainIntroduce'
 import css from './index.scss';
 const cx = classnames.bind(css);
 const moduleName = 'Main';
@@ -11,8 +9,7 @@ class Main extends Component {
   render(){
     return(
       <div className={cx(`${moduleName}`)}>
-        {/* <MainIntroduce />
-        <MainContent /> */}
+        메인페이지를 꾸미자
       </div>
     );
   }
