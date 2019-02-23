5e71c17ed0975cbdb448528574887ae531bd26d5
ERTuringLee <ehdspt@naver.com>
2019-02-23 22:49:12 +0900

    🔨: card router 수정

--- backend/routes/card.js
+++ backend/routes/card.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://yeramdri.com:27017/platform', {useNewUrlParser: true});
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = "mongodb://yeramdri.com:27017/platform"
 
@@ -10,10 +8,14 @@ router.get('/result', function (request, response) {
     MongoClient.connect(mongoUrl, {useNewUrlParser: true}, function (err, mongodb){
         if (err) throw err;
         const DB = mongodb.db('platform');
-        const mySort = { created_at: -1}
+        const mySort = { created_at: -1 }
         search = search.trim();
         if (search == "" || search == "undefined") {
-            response.send("Not Contents")
+            DB.collection('card').find({}).sort(mySort).toArray(function (err, result) {
+                if (err) throw err;
+                response.send(result)
+            })
+            // response.send("Not Contents")
         } else {
             DB.collection('card').find({tag: new RegExp(search, 'i')}).sort(mySort).toArray(function (err, result) {
                 if (err) throw err;
