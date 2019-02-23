b420d068680c4fcdcc4fefc46124f3f1e7ddec45
ERTuringLee <ehdspt@naver.com>
2019-02-23 23:52:13 +0900

    🔨:route 수정

--- backend/routes/card.js
+++ backend/routes/card.js
@@ -3,30 +3,54 @@ const router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = "mongodb://yeramdri.com:27017/platform"
 
-router.get('/result', function (request, response) {
-    let search = request.query.search;
+router.post('/', function (request, response) {
+    let search = request.body.search
     MongoClient.connect(mongoUrl, {useNewUrlParser: true}, function (err, mongodb){
-        if (err) throw err;
-        const DB = mongodb.db('platform');
-        const mySort = { created_at: -1 }
-        DB.collection('card').find({}).sort(mySort).toArray(function (err, result) {
-            if (err) throw err;
-            response.send(result)
+      if (err) throw err;
+      const DB = mongodb.db('platform')
+      if (search == "" || search == "undefined") {
+        DB.collection('card').find({}).toArray(function (err, result) {
+          if (err) throw err;
+          response.send(result)
         })
+      } else {
+        DB.collection('card').find({tag: new RegExp(search, 'i')}).toArray(function (err, result) {
+          if (err) throw err;
+          response.send(result)
+        })
+      }
     })
-});
+  })
+
+  router.get('/result', function (request, response) {
+    let search = request.query.search
+    MongoClient.connect(mongoUrl, {useNewUrlParser: true}, function (err, mongodb){
+      if (err) throw err;
+      const DB = mongodb.db('platform')
+      const mySort = { id: -1 } 
+      search = search.trim()
+      if (search == "" || search == "undefined") {
+        response.send("Not Contents")
+      } else {
+        DB.collection('card').find({tag: new RegExp(search, 'i')}).sort(mySort).toArray(function (err, result) {
+          if (err) throw err;
+          response.send(result)
+        })
+      }
+    })
+  })
 
 router.get('/result/:id', function (request, response) {
     let card_id = parseInt(request.params.id);
     MongoClient.connect(mongoUrl, {useNewUrlParser: true}, function (err, mongodb){
+      if (err) throw err;
+      const DB = mongodb.db('platform')
+      const mySort = { id: -1 }
+      DB.collection('card').find({id: card_id}).sort(mySort).toArray(function (err, result) {
         if (err) throw err;
-        const DB = mongodb.db('platform')
-        const mySort = { created_at: -1}
-        DB.collection('card').find({id: card_id}).sort(mySort).toArray(function (err, result) {
-            if (err) throw err;
-            response.send(result)
-        })
+        response.send(result)
+      })
     })
-})
+  })
 
 module.exports = router;
