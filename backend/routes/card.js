8328d7193bd7f518d39e9b0a79ed31b3077fa9f0
ERTuringLee <ehdspt@naver.com>
2019-02-23 18:16:52 +0900

    [#27] 모든 카드들의 데이터를 조회하는 API생성

---
+++ backend/routes/card.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://yeramdri.com:27017/platform', {useNewUrlParser: true});
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = "mongodb://yeramdri.com:27017/platform"
+
+router.get('/result', function (request, response) {
+    let search = request.query.search;
+    MongoClient.connect(mongoUrl, {useNewUrlParser: true}, function (err, mongodb){
+        if (err) throw err;
+        const DB = mongodb.db('platform');
+        const mySort = { created_at: -1}
+        search = search.trim();
+        if (search == "" || search == "undefined") {
+            response.send("Not Contents")
+        } else {
+            DB.collection('card').find({tag: new RegExp(search, 'i')}).sort(mySort).toArray(function (err, result) {
+                if (err) throw err;
+                response.send(result)
+            })
+        }
+    })
+});
+
+router.get('/result/:id', function (request, response) {
+    let card_id = parseInt(request.params.id);
+    MongoClient.connect(mongoUrl, {useNewUrlParser: true}, function (err, mongodb){
+        if (err) throw err;
+        const DB = mongodb.db('platform')
+        const mySort = { created_at: -1}
+        DB.collection('card').find({id: card_id}).sort(mySort).toArray(function (err, result) {
+            if (err) throw err;
+            response.send(result)
+        })
+    })
+})
+
+module.exports = router;
