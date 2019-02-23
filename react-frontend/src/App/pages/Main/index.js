632f260b8934ceab8775e86c5b49398654889d4c
abelko <esung1129@gmail.com>
2019-02-23 17:18:28 +0900

    :art:[#25]button 컴포넌트UI 변경

--- react-frontend/src/App/pages/Main/index.js
+++ react-frontend/src/App/pages/Main/index.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import classnames from 'classnames/bind'
 import SearchBar from 'src/components/SearchBar';
+import bibleImg from 'src/assets/bible-card.jpg';
+import lifeImg from 'src/assets/life-card2.jpg';
+import ministryImg from 'src/assets/ministry-card.jpg';
 import css from './index.scss'
 const cx = classnames.bind(css)
 const moduleName = 'Main'
@@ -13,33 +16,44 @@ class Main extends Component {
         <div className={cx(`${moduleName}-words`)}>
           <h1 className={cx(`${moduleName}-words-title`)}>예람드리</h1>
           <p className={cx(`${moduleName}-words-subTitle`)}>
-            예배자들의 삶이<br/>아름드리 꽃피우길
+            예배자들의 삶이<br />아름드리 꽃피우길
           </p>
         </div>
         <div className={cx(`${moduleName}-searchbarWrapper`)}>
-          <SearchBar placeholder="#말씀, #삶, #사역"/>
+          <SearchBar placeholder="#말씀, #삶, #사역" />
         </div>
         <div className={cx(`${moduleName}-buttonContainer`)}>
           <Link to="/bible">
-            <div className={cx(`${moduleName}-button`, 'bible')}>
-              <h3>말씀</h3>
-              <i className="fas fa-angle-right" />
+            <div className={cx(`${moduleName}-button`)}>
+              <div className={cx(`${moduleName}-button-imgWrapper`)}>
+                <img src={bibleImg} alt='bibleImg' />
+              </div>
+              <h3 className={cx("bible")}>말씀</h3>
+              <p>예수, 나를 향한 사랑의 시작</p>
             </div>
           </Link>
           <Link to="/life">
             <div className={cx(`${moduleName}-button`, 'life')}>
+              <div className={cx(`${moduleName}-button-imgWrapper`)}>
+                <img src={lifeImg} alt='lifeImg' />
+              </div>
               <h3>삶</h3>
-              <i className="fas fa-angle-right" />
+              <p>일상을 살아가는 크리스천 청년</p>
             </div>
           </Link>
-          <div
-            className={cx(`${moduleName}-button`, 'ministry')}
-            onClick={() => {
-              alert('준비중 입니다 :)')
-            }}
-          >
-            <h3>사역</h3>
-            <i className="fas fa-angle-right" />
+          <div>
+            <div
+              className={cx(`${moduleName}-button`, `${moduleName}-button-last`, 'ministry')}
+              onClick={() => {
+                alert('준비중 입니다 :)')
+              }}
+            >
+              <div className={cx(`${moduleName}-button-imgWrapper`)}>
+                <img src={ministryImg} alt='ministryImg' />
+              </div>
+              <h3>사역</h3>
+              <p>예수, 나를 향한 사랑의 시작</p>
+            </div>
           </div>
         </div>
       </div>
