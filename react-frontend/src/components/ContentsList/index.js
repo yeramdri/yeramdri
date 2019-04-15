e0cab53990172ec018429a1e37e305be986c17ee
Abel <esung1129@gmail.com>
2019-04-15 10:34:31 +0900

    :lipstick:[#55]ContentsList 미디어 쿼리 대응 및 애니메이션 추가

--- react-frontend/src/components/ContentsList/index.js
+++ react-frontend/src/components/ContentsList/index.js
@@ -1,6 +1,6 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import classnames from 'classnames/bind';
 import ContentCard from 'src/components/ContentCard'
 import css from './index.scss';
@@ -8,39 +8,41 @@ const cx = classnames.bind(css);
 const moduleName = 'ContentsList';
 
 class ContentsList extends PureComponent {
-  state = {contentsNumber: 3};
+  state = { contentsNumber: 3 };
 
   _isHideArrow = () => {
-    const {contentsNumber} = this.state
-    const {contents} = this.props;
+    const { contentsNumber } = this.state
+    const { contents } = this.props;
     return contents.length <= contentsNumber
   }
 
   _showMoreContents = () => {
-    this.setState({contentsNumber: this.state.contentsNumber + 3})
+    this.setState({ contentsNumber: this.state.contentsNumber + 3 })
   }
 
   _renderContentCards = () => {
-    const {contentsNumber} = this.state
-    const {contents} = this.props
+    const { contentsNumber } = this.state
+    const { contents } = this.props
     return contents.slice(0, contentsNumber).map(content => (
       <ContentCard key={content._id} content={content} />
     ))
   };
 
   render() {
-    const {contentsState: {pending, fulfilled, rejected}, title} = this.props;
+    const { contentsState: { pending, fulfilled, rejected }, title } = this.props;
     return (
       <div className={cx(`${moduleName}`)}>
-        <p className={cx(`${moduleName}-title`)}>{title}</p>
-        <div>
-          {pending && <div>Loading</div>}
-          {rejected && <div>Error</div>}
-          {fulfilled && this._renderContentCards()}
-          <div className={cx(`${moduleName}-downIcon`, {hide: this._isHideArrow()})}>
-            <i
-              className="fas fa-chevron-down"
-              onClick={this._showMoreContents} />
+        <div className={cx(`${moduleName}-wrapper`)}>
+          <p className={cx(`${moduleName}-title`)}>{title}</p>
+          <div>
+            {pending && <div>Loading</div>}
+            {rejected && <div>Error</div>}
+            {fulfilled && this._renderContentCards()}
+            <div className={cx(`${moduleName}-downIcon`, { hide: this._isHideArrow() })}>
+              <i
+                className="fas fa-chevron-down"
+                onClick={this._showMoreContents} />
+            </div>
           </div>
         </div>
       </div>
@@ -54,7 +56,7 @@ ContentsList.propTypes = {
   title: PropTypes.string
 }
 
-const mapStateToProps = ({contents: {contentsState, contents}}) => ({contentsState, contents});
+const mapStateToProps = ({ contents: { contentsState, contents } }) => ({ contentsState, contents });
 const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContentsList);
