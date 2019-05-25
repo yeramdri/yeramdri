cd3f805d179ee635c72364cfb23a7b0a4e1ae44a
Abel <esung1129@gmail.com>
2019-05-25 11:40:44 +0900

    :sparkles:img 업로드 하는 기본 컴포너트 추가

--- react-frontend/src/App/pages/Create/index.jsx
+++ react-frontend/src/App/pages/Create/index.jsx
@@ -3,30 +3,46 @@ import PropTypes from 'prop-types';
 
 class Create extends Component {
   state = {
-    selectedFile: null
+    selectedFile: null,
+    imgPreviewUrl: null
   }
 
-  fileSelectedHandler = e => {
-    this.setState({ selectedFile: e.target.files[0] })
+  _handleFileSelect = e => {
+    e.preventDefault();
+    let file = e.target.files[0]
+    let reader = new FileReader();
+
+    reader.onloadend = () => {
+      this.setState({ selectedFile: file, imgPreviewUrl: reader.result });
+    }
+
+    reader.readAsDataURL(file)
   }
 
-  fileUploadHandler = () => {
+  _handleFileUpload = () => {
     const { selectedFile, selectedFile: { name } } = this.state;
     const fd = new FormData();
     fd.append('image', selectedFile, name);
     // api요청. axios의 네번째 인자로 콜백을 보내면, img업로드 퍼센트를 얻을 수 있다.
   }
 
+  _renderPrevImg = imgSrc => {
+    return <img src={imgSrc}></img>
+  }
+
   render() {
+    const { imgPreviewUrl } = this.state;
     return (
       <div>
+        {imgPreviewUrl && this._renderPrevImg(imgPreviewUrl)}
         <input
           style={{ display: 'none' }}
           type="file"
-          onChange={this.fileSelectedHandler}
+          multiple="multiple"
+          onChange={this._handleFileSelect}
           ref={fileInput => this.fileInput = fileInput} />
         <button onClick={() => this.fileInput.click()}>Pick</button>
-        <button onClick={this.fileUploadHandler}>Create</button>
+        <button onClick={this._handleFileUpload}>Create</button>
       </div>
     );
   }
