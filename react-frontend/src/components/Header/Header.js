5f568e1b6863dd3f76cf7f68537b6a76355ab5a5
juchanhwang <ghkdwncks96@gmail.com>
2019-05-25 16:15:22 +0900

    :lipstick: make Header sticky/ 헤더 영역 position: fixed, 그로 인해 생기는 겹침 현상은 상위에 fake-layer 영역 추가, opacity:0.8

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import classnames from 'classnames/bind'
+import React from "react";
+import { Link } from "react-router-dom";
+import classnames from "classnames/bind";
 
-import yeramdriLogo from 'src/assets/yeramdri-logo.svg'
+import yeramdriLogo from "src/assets/yeramdri-logo.svg";
 
-import css from './Header.scss'
-const cx = classnames.bind(css)
-const moduleName = 'Header'
+import css from "./Header.scss";
+const cx = classnames.bind(css);
+const moduleName = "Header";
 
 const Header = ({ toggleSidebar }) => {
   return (
-    <div className={cx(`${moduleName}`)}>
-      <div className={cx(`${moduleName}-title-logo-wrapper`)}>
-        <Link className={cx(`${moduleName}-logo`)} to="/">
-          <img src={yeramdriLogo} alt="logo" />
-        </Link>
-        <Link to="/">
-          <h1>Yeramdri</h1>    
-        </Link>
+    <div className={cx(`${moduleName}-fake-layer`)}>
+      <div className={cx(`${moduleName}`)}>
+        <div className={cx(`${moduleName}-title-logo-wrapper`)}>
+          <Link className={cx(`${moduleName}-logo`)} to="/">
+            <img src={yeramdriLogo} alt="logo" />
+          </Link>
+          <Link to="/">
+            <h1>Yeramdri</h1>
+          </Link>
+        </div>
+        <i onClick={toggleSidebar} className="fas fa-bars" />
       </div>
-      <i onClick={toggleSidebar} className="fas fa-bars" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
