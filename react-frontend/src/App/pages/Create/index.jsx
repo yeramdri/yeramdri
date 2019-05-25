4e6948aea54f91fc666aa32f1a243528a915839d
Abel <esung1129@gmail.com>
2019-05-25 14:28:58 +0900

    :sparkles:데이터 input 마크업 추가

--- react-frontend/src/App/pages/Create/index.jsx
+++ react-frontend/src/App/pages/Create/index.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Create extends Component {
-  state = {
-    selectedFileList: [],
-    imgPreviewUrlList: []
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedFileList: [],
+      imgPreviewUrlList: [],
+      category: '',
+      bibleSection: '',
+      scripture: '',
+      description: '',
+      tag: '',
+    }
+    this.fileInput = null;
+  }
+
+  _handleChange = e => {
+    const { name, value } = e.target;
+    this.setState(() => ({ [name]: value }));
   }
 
   _handleFileSelect = e => {
@@ -50,6 +64,28 @@ class Create extends Component {
           onChange={this._handleFileSelect}
           ref={fileInput => this.fileInput = fileInput} />
         <button onClick={() => this.fileInput.click()}>Pick</button>
+        <select name="category" onChange={this._handleChange}>
+          <option value="bible">말씀</option>
+          <option value="life">삶</option>
+        </select>
+        <input
+          name="bibleSection"
+          onChange={this._handleChange}
+          type="text"
+          placeholder="말씀 구간을 입력해 주세요" />
+        <textarea
+          name="scripture"
+          onChange={this._handleChange}
+          placeholder="말씀을 입력해 주세요" />
+        <textarea
+          name="description"
+          onChange={this._handleChange}
+          placeholder="설명 또는 나눔을 입력해 주세요" />
+        <input
+          name="tag"
+          onChange={this._handleChange}
+          type="text"
+          placeholder="태그를 입력해 주세요(#을 사용해 주세요)" />
         <button onClick={this._handleFileUpload}>Create</button>
       </div>
     );
